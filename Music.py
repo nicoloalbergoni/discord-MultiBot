219814f8d404128eba93e0908bc27d3e7a949659
Nicolò Albergoni <albergoni.nicolo@gmail.com>
2019-07-14 14:57:17 +0200

    Test custom logger

--- Music.py
+++ Music.py
@@ -7,8 +7,8 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def dc(self, ctx):
+    @commands.command(name='dc')
+    async def disconnect(self, ctx):
         """Stops and disconnects the bot from voice"""
         logging.info(f'Disconnected from voice channel: {ctx.voice_client.channel.name}')
         await ctx.voice_client.disconnect()
@@ -23,7 +23,7 @@ class Music(commands.Cog):
 
         await ctx.send('Now playing: {}'.format(player.title))
 
-    @commands.command()
+    @commands.command(name='p')
     async def play(self, ctx, *, url):
         """Streams from a url (same as yt, but doesn't predownload)"""
         async with ctx.typing():
@@ -32,6 +32,14 @@ class Music(commands.Cog):
                 'Player error: %s' % e) if e else None)
 
         await ctx.send('Now playing: {}'.format(player.title))
+
+    @commands.command()
+    async def skip(self, ctx):
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+
+
 
     @play.before_invoke
     async def ensure_voice(self, ctx):
--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
 from discord.ext import commands
+from bot_logger import Logger
 from secret import BOT_TOKEN
 import logging
 from Music import Music
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(process)d-%(levelname)s-%(message)s')
+logging.basicConfig(level=logging.MESSAGE,
+                  format='%(process)d-%(levelname)s-%(message)s')
+
+logging.setLoggerClass(Logger)
+logger = logging.getLogger('Bot Logger')
+logger.message('This seems to work')
 
 bot = commands.Bot(command_prefix='!')
 
 
 @bot.event
 async def on_ready():
-    logging.info(f'Logged in as {bot.user.name}')
+    logger.info(f'Logged in as {bot.user.name}')
 
 
 @bot.command()
 async def test(ctx, url: str, time: str = None):
-    logging.info(
+    logger.info(
         f'User {ctx.author.name} called the {ctx.command.name} command')
     # channel = ctx.author.voice.channel
     # vc = await channel.connect()
