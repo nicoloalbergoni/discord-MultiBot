aa575f172d6bb2cde5718c97eefa963444d1c2bf
Nicolò Albergoni <nicolo.albe@gmail.com>
2019-07-17 18:38:55 +0200

    Added join method

--- Music.py
+++ Music.py
@@ -13,12 +13,18 @@ class Music(commands.Cog):
     @commands.command(name='dc')
     async def disconnect(self, ctx):
         """Stops and disconnects the bot from voice"""
-        logger.info(f'Disconnected from voice channel: {ctx.voice_client.channel.name}')
-        await ctx.voice_client.disconnect()
+        if ctx.voice_client is not None:
+            logger.info(
+                f'Disconnected from voice channel: {ctx.voice_client.channel.name}')
+            await ctx.voice_client.disconnect()
+        else:
+            await ctx.send('Not connected to any voice channel')
 
     @commands.command(name='p')
     async def play(self, ctx, *, url):
         """Streams from a url (same as yt, but doesn't predownload)"""
+        logger.info(
+            f'User {ctx.author.name} requested song: {url}')
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
             ctx.voice_client.play(player, after=lambda e: print(
@@ -33,14 +39,18 @@ class Music(commands.Cog):
         else:
             # TODO: fixare title attribute not found
             ctx.voice_client.stop()
-            logger.info(f'{ctx.author.name} skipped song {ctx.voice_client.source.title}')
+            logger.info(
+                f'{ctx.author.name} skipped song {ctx.voice_client.source.title}')
             await ctx.send(f'Skipped song: {ctx.voice_client.source.title}')
 
+    @commands.command(name='join')
     @play.before_invoke
-    async def ensure_voice(self, ctx):
+    async def join_channel(self, ctx):
         if ctx.voice_client is None:
             if ctx.author.voice:
                 await ctx.author.voice.channel.connect()
+                logger.info(
+                    f'Connected to voice channel {ctx.voice_client.channel.name}')
             else:
                 await ctx.send("You are not connected to a voice channel.")
                 raise commands.CommandError(
